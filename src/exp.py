676b0622c0ae8b074891d65612e2251c186de7f0
EaKal-7 <e5kbb@vip.qq.com>
2023-01-28 19:28:25 +0800

    UPDATE exp.py && download moudle

--- src/exp.py
+++ src/exp.py
@@ -19,30 +19,26 @@
 ################################################################################
 import sys
 import json
-
+import urllib
+import random
+import string
+import requests
 from pytube import YouTube
+
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file '1MHOmam.ui'
+## Form generated from reading UI file '1AXESWc.ui'
 ##
 ## Created by: Qt User Interface Compiler version 5.14.2
 ##
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
 
-# -*- coding: utf-8 -*-
-
-################################################################################
-## Form generated from reading UI file '1njtTWh.ui'
-##
-## Created by: Qt User Interface Compiler version 5.14.2
-##
-## WARNING! All changes made in this file will be lost when recompiling UI file!
-################################################################################
+yt=None
 
 from PySide2.QtCore import (QCoreApplication, QDate, QDateTime, QMetaObject,
-    QObject, QPoint, QRect, QSize, QTime, QUrl, Qt)
+    QObject, QPoint, QRect, QSize, QTime, QUrl, Qt,QThread,Signal,QObject)
 from PySide2.QtGui import (QBrush, QColor, QConicalGradient, QCursor, QFont,
     QFontDatabase, QIcon, QKeySequence, QLinearGradient, QPalette, QPainter,
     QPixmap, QRadialGradient)
@@ -53,13 +49,13 @@ class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(894, 784)
+        MainWindow.resize(1140, 895)
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
         self.verticalLayoutWidget = QWidget(self.centralwidget)
         self.verticalLayoutWidget.setObjectName(u"verticalLayoutWidget")
         self.verticalLayoutWidget.setEnabled(True)
-        self.verticalLayoutWidget.setGeometry(QRect(0, 0, 731, 451))
+        self.verticalLayoutWidget.setGeometry(QRect(0, 0, 1069, 887))
         self.verticalLayout = QVBoxLayout(self.verticalLayoutWidget)
         self.verticalLayout.setObjectName(u"verticalLayout")
         self.verticalLayout.setContentsMargins(0, 1, 0, 0)
@@ -90,14 +86,82 @@ class Ui_MainWindow(object):
 
         self.verticalLayout.addLayout(self.horizontalLayout_4)
 
-        self.verticalSpacer = QSpacerItem(20, 1800, QSizePolicy.Minimum, QSizePolicy.Expanding)
+        self.gridLayout = QGridLayout()
+        self.gridLayout.setObjectName(u"gridLayout")
+        self.horizontalSpacer_2 = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
 
-        self.verticalLayout.addItem(self.verticalSpacer)
+        self.gridLayout.addItem(self.horizontalSpacer_2, 1, 3, 1, 1)
 
-        self.pushButton = QPushButton(self.verticalLayoutWidget)
-        self.pushButton.setObjectName(u"pushButton")
+        self.KeywordPlain = QLabel(self.verticalLayoutWidget)
+        self.KeywordPlain.setObjectName(u"KeywordPlain")
+        font1 = QFont()
+        font1.setPointSize(12)
+        self.KeywordPlain.setFont(font1)
+        self.KeywordPlain.setAlignment(Qt.AlignCenter)
 
-        self.verticalLayout.addWidget(self.pushButton)
+        self.gridLayout.addWidget(self.KeywordPlain, 2, 1, 1, 1)
+
+        self.PicPlain = QLabel(self.verticalLayoutWidget)
+        self.PicPlain.setObjectName(u"PicPlain")
+        self.PicPlain.setFont(font1)
+        self.PicPlain.setAlignment(Qt.AlignCenter)
+
+        self.gridLayout.addWidget(self.PicPlain, 3, 1, 1, 1)
+
+        self.ViewsEdit = QLabel(self.verticalLayoutWidget)
+        self.ViewsEdit.setObjectName(u"ViewsEdit")
+        self.ViewsEdit.setFont(font1)
+        self.ViewsEdit.setAlignment(Qt.AlignLeading|Qt.AlignLeft|Qt.AlignVCenter)
+        self.ViewsEdit.setWordWrap(False)
+
+        self.gridLayout.addWidget(self.ViewsEdit, 1, 2, 1, 1)
+
+        self.KeywordEdit = QLabel(self.verticalLayoutWidget)
+        self.KeywordEdit.setObjectName(u"KeywordEdit")
+        self.KeywordEdit.setFont(font1)
+        self.KeywordEdit.setAlignment(Qt.AlignLeading|Qt.AlignLeft|Qt.AlignVCenter)
+        self.KeywordEdit.setWordWrap(False)
+
+        self.gridLayout.addWidget(self.KeywordEdit, 2, 2, 1, 1)
+
+        self.viewsPlain = QLabel(self.verticalLayoutWidget)
+        self.viewsPlain.setObjectName(u"viewsPlain")
+        self.viewsPlain.setFont(font1)
+        self.viewsPlain.setAlignment(Qt.AlignCenter)
+
+        self.gridLayout.addWidget(self.viewsPlain, 1, 1, 1, 1)
+
+        self.titleEdit = QLabel(self.verticalLayoutWidget)
+        self.titleEdit.setObjectName(u"titleEdit")
+        self.titleEdit.setMinimumSize(QSize(600, 0))
+        self.titleEdit.setFont(font1)
+        self.titleEdit.setAlignment(Qt.AlignLeading|Qt.AlignLeft|Qt.AlignVCenter)
+        self.titleEdit.setWordWrap(False)
+
+        self.gridLayout.addWidget(self.titleEdit, 0, 2, 1, 1)
+
+        self.horizontalSpacer = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
+
+        self.gridLayout.addItem(self.horizontalSpacer, 1, 0, 1, 1)
+
+        self.titlePlain = QLabel(self.verticalLayoutWidget)
+        self.titlePlain.setObjectName(u"titlePlain")
+        self.titlePlain.setMaximumSize(QSize(60, 16777215))
+        self.titlePlain.setFont(font1)
+        self.titlePlain.setAlignment(Qt.AlignCenter)
+
+        self.gridLayout.addWidget(self.titlePlain, 0, 1, 1, 1)
+
+        self.PicEdit = QLabel(self.verticalLayoutWidget)
+        self.PicEdit.setObjectName(u"PicEdit")
+        self.PicEdit.setFont(font1)
+        self.PicEdit.setAlignment(Qt.AlignLeading|Qt.AlignLeft|Qt.AlignVCenter)
+        self.PicEdit.setWordWrap(False)
+
+        self.gridLayout.addWidget(self.PicEdit, 3, 2, 1, 1)
+
+
+        self.verticalLayout.addLayout(self.gridLayout)
 
         self.result = QLabel(self.verticalLayoutWidget)
         self.result.setObjectName(u"result")
@@ -106,16 +170,55 @@ class Ui_MainWindow(object):
 
         self.tableWidget = QTableWidget(self.verticalLayoutWidget)
         self.tableWidget.setObjectName(u"tableWidget")
-        self.tableWidget.setMinimumSize(QSize(0, 600))
+        self.tableWidget.setMinimumSize(QSize(0, 300))
+        self.tableWidget.setLayoutDirection(Qt.LeftToRight)
+        self.tableWidget.setSelectionMode(QAbstractItemView.SingleSelection)
+        self.tableWidget.setSelectionBehavior(QAbstractItemView.SelectRows)
 
         self.verticalLayout.addWidget(self.tableWidget)
 
-        self.pushButton_2 = QPushButton(self.verticalLayoutWidget)
-        self.pushButton_2.setObjectName(u"pushButton_2")
-        self.pushButton_2.setEnabled(True)
+        self.gridLayout_2 = QGridLayout()
+        self.gridLayout_2.setObjectName(u"gridLayout_2")
+        self.FlieSelect = QPushButton(self.verticalLayoutWidget)
+        self.FlieSelect.setObjectName(u"FlieSelect")
 
-        self.verticalLayout.addWidget(self.pushButton_2)
-        self.pushButton_2.clicked.connect(self.clean)
+        self.gridLayout_2.addWidget(self.FlieSelect, 1, 2, 1, 1)
+
+        self.DownloadPath = QTextEdit(self.verticalLayoutWidget)
+        self.DownloadPath.setObjectName(u"DownloadPath")
+        self.DownloadPath.setMaximumSize(QSize(16777215, 40))
+        self.DownloadPath.setFont(font)
+
+        self.gridLayout_2.addWidget(self.DownloadPath, 1, 1, 1, 1)
+
+        self.DownloadPathPlain = QLabel(self.verticalLayoutWidget)
+        self.DownloadPathPlain.setObjectName(u"DownloadPathPlain")
+
+        self.gridLayout_2.addWidget(self.DownloadPathPlain, 1, 0, 1, 1)
+
+
+        self.verticalLayout.addLayout(self.gridLayout_2)
+
+        self.pushButton = QPushButton(self.verticalLayoutWidget)
+        self.pushButton.setObjectName(u"pushButton")
+
+        self.verticalLayout.addWidget(self.pushButton)
+
+        self.progressBar = QProgressBar(self.verticalLayoutWidget)
+        self.progressBar.setObjectName(u"progressBar")
+        self.progressBar.setValue(0)
+
+        self.verticalLayout.addWidget(self.progressBar)
+        self.label = QLabel(self.verticalLayoutWidget)
+        self.label.setObjectName(u"label")
+
+        self.verticalLayout.addWidget(self.label)
+
+        self.verticalSpacer_2 = QSpacerItem(20, 600, QSizePolicy.Minimum, QSizePolicy.Maximum)
+
+        self.verticalLayout.addItem(self.verticalSpacer_2)
+        self.pushButton.clicked.connect(self.download)
+        self.FlieSelect.clicked.connect(self.openexploer)
         self.Button.clicked.connect(self.show)
         MainWindow.setCentralWidget(self.centralwidget)
 
@@ -128,62 +231,158 @@ class Ui_MainWindow(object):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
         self.textview.setText(QCoreApplication.translate("MainWindow", u"\u8bf7\u8f93\u5165\u7f51\u5740", None))
         self.Button.setText(QCoreApplication.translate("MainWindow", u"\u89e3\u6790", None))
-        self.pushButton.setText(QCoreApplication.translate("MainWindow", u"PushButton", None))
+        self.KeywordPlain.setText(QCoreApplication.translate("MainWindow", u"Keyword:", None))
+        self.PicPlain.setText(QCoreApplication.translate("MainWindow", u"Pic:", None))
+        self.ViewsEdit.setText(QCoreApplication.translate("MainWindow", u"TextLabel", None))
+        self.KeywordEdit.setText(QCoreApplication.translate("MainWindow", u"TextLabel", None))
+        self.viewsPlain.setText(QCoreApplication.translate("MainWindow", u"Views:", None))
+        self.titleEdit.setText(QCoreApplication.translate("MainWindow", u"TextLabel", None))
+        self.titlePlain.setText(QCoreApplication.translate("MainWindow", u"Title", None))
+        self.PicEdit.setText(QCoreApplication.translate("MainWindow", u"TextLabel", None))
         self.result.setText(QCoreApplication.translate("MainWindow", u"TextLabel", None))
-        self.pushButton_2.setText(QCoreApplication.translate("MainWindow", u"PushButton", None))
+        self.FlieSelect.setText(QCoreApplication.translate("MainWindow", u"FlieSelect", None))
+        self.DownloadPathPlain.setText(QCoreApplication.translate("MainWindow", u"DownLoadPath", None))
+        self.pushButton.setText(QCoreApplication.translate("MainWindow", u"DownLoad!!!", None))
+        self.label.setText(QCoreApplication.translate("MainWindow", u"TextLabel", None))
     # retranslateUi
 
+
+
+
+    def openexploer(self):
+        self.DownloadPath.setText(QFileDialog.getExistingDirectory())
 
     def clean(self):
         self.tableWidget.clear()
 
     def show(self):
+        global yt
         url = self.textEdit.toPlainText()
         result={}
         print(url)
-        # try:
-        #     # ,proxies={"https":"127.0.0.1:7890","http":"127.0.0.1:7890"}
-        #     yt = YouTube(url)
-        # except Exception as e:
-        #     return {"error": str(e)}
-        # result["title"] = yt.title
-        # result["streams"] = []
-        # result["pic"] = yt.thumbnail_url
-        # result["views"] = yt.views
-        # result["keyword"] = yt.keywords
-        # temp={}
-        # for i in yt.streams.filter(file_extension='mp4',progressive=True):
-        #     temp["itag"] = i.itag
-        #     try:
-        #         temp["resolution"] = i.resolution
-        #     except:
-        #         temp["resolution"] = "None"
-        #     temp["fps"] = i.fps
-        #     temp["vcodec"] = i.video_codec
-        #     temp["filesize"] = i.filesize_mb
-        #     temp["download_url"] = i.url
-        #     result["streams"].append(temp.copy())
-        with open ("new.json","r") as f:
-            result=json.load(f)
+        try:
+            # 
+            yt = YouTube(url)
+        except Exception as e:
+            return {"error": str(e)}
+        result["title"] = yt.title
+        result["streams"] = []
+        result["pic"] = yt.thumbnail_url
+        result["views"] = yt.views
+        result["keyword"] = yt.keywords
+        temp={}
+        for i in yt.streams.filter(file_extension='mp4',progressive=True):
+            temp["itag"] = i.itag
+            try:
+                temp["resolution"] = i.resolution
+            except:
+                temp["resolution"] = "None"
+            temp["fps"] = i.fps
+            temp["vcodec"] = i.video_codec
+            temp["filesize"] = i.filesize_mb
+            temp["download_url"] = i.url
+            result["streams"].append(temp.copy())
+        # with open ("new.json","r") as f:
+        #     result=json.load(f)
+        self.titleEdit.setText(result["title"])
+        self.ViewsEdit.setText(str(result["views"]))
+        self.KeywordEdit.setText(str(result["keyword"]))
+        req=requests.get(result['pic'])
+        with open('./temp/'+url.split('v=')[-1]+'.jpg','wb') as f:
+            f.write(req.content)
+        self.PicEdit.setPixmap(QPixmap('./temp/'+url.split('v=')[-1]+'.jpg'))
         self.tableWidget.setRowCount(0)
         self.tableWidget.clear()
         self.tableWidget.setColumnCount(6)
+        # self.tableWidget.setEditsetEditTriggers(QAbstractItemView.NoEditTriggers)
+        # self.tableWidget.setSectionResizeMode(QHeaderView.Stretch)
+        self.tableWidget.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
         titlelist=["itag","resolution","fps","vcodec","filesize","download_url"]
         self.tableWidget.setHorizontalHeaderLabels(titlelist)
         # print(result["streams"])
         sum=0
         for i in result["streams"]:
             self.tableWidget.insertRow(sum)
-            self.tableWidget.setItem(sum,0,QTableWidgetItem(str(i["itag"])))
-            print(str(i["itag"]))
-            self.tableWidget.setItem(sum,1,QTableWidgetItem(str(i["resolution"])))
-            self.tableWidget.setItem(sum,2,QTableWidgetItem(str(i["fps"])))
-            self.tableWidget.setItem(sum,3,QTableWidgetItem(str(i["vcodec"])))
-            self.tableWidget.setItem(sum,4,QTableWidgetItem(str(i["filesize"])))
-            self.tableWidget.setItem(sum,5,QTableWidgetItem(str(i["download_url"])))
+            itag = QTableWidgetItem(str(i["itag"]))
+            resolution = QTableWidgetItem(str(i["resolution"]))
+            fps = QTableWidgetItem(str(i["fps"]))
+            vcodec = QTableWidgetItem(str(i["vcodec"]))
+            filesize = QTableWidgetItem(str(i["filesize"]))
+            download_url = QTableWidgetItem(str(i["download_url"]))
+            itag.setFlags( Qt.ItemIsEnabled | Qt.ItemIsUserCheckable | Qt.ItemIsSelectable | Qt.ItemIsDragEnabled | Qt.ItemIsDropEnabled)
+            resolution.setFlags( Qt.ItemIsEnabled | Qt.ItemIsUserCheckable | Qt.ItemIsSelectable | Qt.ItemIsDragEnabled | Qt.ItemIsDropEnabled)
+            fps.setFlags( Qt.ItemIsEnabled | Qt.ItemIsUserCheckable | Qt.ItemIsSelectable | Qt.ItemIsDragEnabled | Qt.ItemIsDropEnabled)
+            vcodec.setFlags( Qt.ItemIsEnabled | Qt.ItemIsUserCheckable | Qt.ItemIsSelectable | Qt.ItemIsDragEnabled | Qt.ItemIsDropEnabled)
+            filesize.setFlags( Qt.ItemIsEnabled | Qt.ItemIsUserCheckable | Qt.ItemIsSelectable | Qt.ItemIsDragEnabled | Qt.ItemIsDropEnabled)
+            download_url.setFlags( Qt.ItemIsEnabled | Qt.ItemIsUserCheckable | Qt.ItemIsSelectable | Qt.ItemIsDragEnabled | Qt.ItemIsDropEnabled)
+            self.tableWidget.setItem(sum,0,itag)
+            self.tableWidget.setItem(sum,1,resolution)
+            self.tableWidget.setItem(sum,2,fps)
+            self.tableWidget.setItem(sum,3,vcodec)
+            self.tableWidget.setItem(sum,4,filesize)
+            self.tableWidget.setItem(sum,5,download_url)
             sum+=1
+        # self.tableWidget.resizeColumnsToContents()
+    
+    def download(self):
+        global yt
+        print(self.tableWidget.currentRow())
+        if yt==None:
+            self.label.setText("请先获取视频信息")
+            return
+        if self.DownloadPath.toPlainText()=="":
+            self.label.setText("请选择下载路径")
+            return
+        if self.tableWidget.currentRow()==-1:
+            self.label.setText("请选择下载视频")
+            return
+        downloadurl=self.tableWidget.item(self.tableWidget.currentRow(),5).text()
+        self.label.setText("正在下载")
+        # yt.streams.get_by_itag(itag).download(self.DownloadPath.toPlainText())
+        self.label.setText("下载完成")
+        the_filesize = requests.get(downloadurl, stream=True).headers['Content-Length']
+        the_filepath = self.DownloadPath.toPlainText()+"/"+yt.title+".mp4"
+        the_fileobj = open(the_filepath, 'wb')
+        #### 创建下载线程
+        self.downloadThread = downloadThread(downloadurl, the_filesize, the_fileobj, buffer=10240)
+        self.downloadThread.download_proess_signal.connect(self.set_progressbar_value)
+        self.downloadThread.start()
+
+    def set_progressbar_value(self, value):
+        self.progressBar.setValue(value)
+        if value == 100:
+            self.label.setText("下载完成")
+            return
+            
+class downloadThread(QThread):
+    download_proess_signal = Signal(int)                        #创建信号
+
+    def __init__(self, url, filesize, fileobj, buffer):
+        super(downloadThread, self).__init__()
+        self.url = url
+        self.filesize = filesize
+        self.fileobj = fileobj
+        self.buffer = buffer
 
 
+    def run(self):
+        try:
+            rsp = requests.get(self.url, stream=True)                #流下载模式
+            offset = 0
+            for chunk in rsp.iter_content(chunk_size=self.buffer):
+                if not chunk: break
+                self.fileobj.seek(offset)                            #设置指针位置
+                self.fileobj.write(chunk)                            #写入文件
+                offset = offset + len(chunk)
+                proess = offset / int(self.filesize) * 100
+                self.download_proess_signal.emit(int(proess))        #发送信号
+            #######################################################################
+            self.fileobj.close()    #关闭文件
+            self.exit(0)            #关闭线程
+
+
+        except Exception as e:
+            print(e)
 
 if __name__ == "__main__":
     # 初始化QApplication，界面展示要包含在QApplication初始化之后，结束之前
